073c7ec0c1cf3dda9c8853f5e709a0b1802d57e7
Matthew Morek <matthew@madbit.co>
2017-12-04 22:33:20 +0000

    🚧 Add LocalStorage support and autosaving
    Use `lodash.debounce` method to introduce rate limits for detecting keystrokes.

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-auto">
-    <aside class="flex flex-auto flex-column w-30 w-25-m mw6">
+    <aside class="flex flex-auto flex-column w-30 w-25-m mw6" v-show="notes.length">
       <div class="flex-auto">
         <div v-for="note in notes" :key="note.id"
           @click="loadNote(note)"
@@ -28,16 +28,41 @@
 </template>
 
 <script>
+import _ from 'lodash';
 import ListItem from '@/components/list-item';
+
+// localStorage persistence
+const STORAGE_KEY = 'scratchpad';
+var notesStorage = {
+  fetch: function () {
+    var notes = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    notes.forEach(function (note, index) {
+      note.id = index;
+    });
+    notesStorage.uid = notes.length;
+    return notes;
+  },
+  save: function (notes) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(notes));
+  }
+};
 
 export default {
   components: { ListItem },
   // Initial data
   data: function () {
     return {
-      notes: [],
+      notes: notesStorage.fetch(),
       editedNote: {}
     };
+  },
+  watch: {
+    notes: {
+      handler: function (notes) {
+        notesStorage.save(notes);
+      },
+      deep: true
+    }
   },
   methods: {
     createNote: function () {
@@ -45,19 +70,22 @@ export default {
       this.$refs.editor.focus();
     },
     // Use lodash's debounce to introduce rate limiting
-    saveNote: function () {
-      var draft = {
-        id: (this.editedNote.id ? this.editedNote.id : notesStorage.uid++),
-        content: this.editedNote.content
-      };
+    saveNote: _.debounce(
+      function () {
+        var draft = {
+          id: (this.editedNote.id ? this.editedNote.id : notesStorage.uid++),
+          content: this.editedNote.content
+        };
 
-      if (!this.editedNote.id && this.editedNote.content && this.notes.indexOf(draft.id) === -1) {
-        var lastId = this.notes.push(draft);
-        this.editedNote = this.notes[lastId - 1];
-      } else {
+        if (!this.editedNote.id && this.editedNote.content && this.notes.indexOf(draft.id) === -1) {
+          var lastId = this.notes.push(draft);
+          this.editedNote = this.notes[lastId - 1];
+        } else {
 
-      }
-    },
+        }
+      },
+      500
+    ),
     loadNote: function (note) {
       this.editedNote = note;
       this.$refs.editor.focus();
